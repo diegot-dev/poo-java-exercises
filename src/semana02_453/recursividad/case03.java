56106f9c0a55a1595b3c611f9bbdece319e3e893
diegot-dev <diegotutaya.dev+github@gmail.com>
2026-09-06 19:52:37 -0500

    upd: case03

--- src/semana02_453/recursividad/case03.java
+++ src/semana02_453/recursividad/case03.java
@@ -1,5 +1,33 @@
 package semana02_453.recursividad;
 
+import java.util.Scanner;
+
 public class case03 {
-    
+    public static int intInput(String txt, Scanner s)
+    {
+        int num;
+        System.out.print(txt + ": ");
+        num = s.nextInt();
+        return num;
+    }
+
+    public static int totalAmount(int currTotal, Scanner s)
+    {
+        int price, quantity, totalAmount = currTotal;
+        price = intInput("Ingrese precio unitario del producto", s);
+        if (price == -1) return totalAmount;
+        quantity = intInput("Ingrese cantidad de productos", s);
+        totalAmount = currTotal + price * quantity;
+        return totalAmount(totalAmount, s);
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("CALCULADOR DE MONTO TOTAL");
+        System.out.println("Ingrese '-1' para terminar");
+        Scanner s = new Scanner(System.in);
+        int num = totalAmount(0, s);
+        System.out.println("Resultado: " + num);
+        s.close();
+    }
 }
---
+++ src/semana02_453/recursividad/case04.java
@@ -0,0 +1,35 @@
+package semana02_453.recursividad;
+import java.util.Scanner;
+import java.io.File;
+public class case04 {
+    public static void print(String txt)
+    {
+        System.out.println(txt);
+    }
+    
+    public static void browseFolder(File folder) {
+        File[] archives = folder.listFiles();
+        if (archives == null) print(folder.getName());
+        for (File archive : archives) {
+            if (archive.isDirectory()) {
+                print("\nCarpeta : " + archive.getAbsolutePath());
+                browseFolder(archive);
+            } else print("Archivo : " + archive.getAbsolutePath());
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Ingrese el directorio: ");
+        String path = s.nextLine();
+        s.close();
+
+        File folder = new File(path);
+        
+        if (folder.exists() && folder.isDirectory()) {
+            browseFolder(folder);
+        } else print("No existe o no es una carpeta");
+        
+    }
+}
