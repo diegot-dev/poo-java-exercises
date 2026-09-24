58b7f2b30e74fd57ad7dfd42a599ac739620821c
diegot-dev <diegotutaya.dev+github@gmail.com>
2026-09-24 15:50:17 -0500

    added ex10

--- src/semana03_453/prop2/ex10.java
+++ src/semana03_453/prop2/ex10.java
@@ -1,13 +1,36 @@
 package semana03_453.prop2;
-/* Estado: no concluido */
+/* Estado: concluido */
 public class ex10 {
-    public static void main(String[] args) {
-        int[] arr = new int[5];
-        int sum = 0;
-        for (int i = 0; i < 5; i++) {
-            arr[i] += sum;
+    public static int[] delRepeated(int[] arr) {
+        int last = arr.length;
+        boolean isUnique;
+        int countUniq = 0;
+        int[] uniques = new int[last];
+
+        for (int i = 0; i < last; i++) {
+            isUnique = true;
+            for (int j = 0; j < countUniq; j++) {
+                if (arr[i] == uniques[j]) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                uniques[countUniq] = arr[i];
+                countUniq++;
+            }
         }
-        System.out.println(sum);
-        
+        /* Creando un array con solo esos elementos */
+        int[] uniqueFinal = new int[countUniq];
+        for (int i = 0; i < countUniq; i++) {
+            uniqueFinal[i] = uniques[i];
+        }
+        return uniqueFinal;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {5,5,5,5,5,5,5,4,5,5,5,2,5,5,3};
+        int[] notRep = delRepeated(nums);
+        for(int elem : notRep) System.out.println(elem);
     }
 }
